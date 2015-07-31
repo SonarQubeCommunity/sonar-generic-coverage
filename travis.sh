03e649c6a454bd508b90ef1522c76303168b7bd7
Massimo Paladin <massimo.paladin@gmail.com>
2015-07-31 14:29:13 +0200

    Switch ITs to SonarQube LTS_OR_OLDEST_COMPATIBLE

--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v15 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v16 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
@@ -25,13 +25,13 @@ IT-DEV)
   mvn -DgenericcoverageVersion="DEV" -DjavascriptVersion="LATEST_RELEASE" -DjavaVersion="LATEST_RELEASE" -Dsonar.runtimeVersion="DEV" -Dmaven.test.redirectTestOutputToFile=false install
   ;;
 
-IT-LATEST)
+IT-LTS_OR_OLDEST_COMPATIBLE)
   installTravisTools
 
   mvn install -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
 
   cd its/plugin
-  mvn -DgenericcoverageVersion="DEV" -DjavascriptVersion="LATEST_RELEASE" -DjavaVersion="LATEST_RELEASE" -Dsonar.runtimeVersion="LATEST_RELEASE" -Dmaven.test.redirectTestOutputToFile=false install
+  mvn -DgenericcoverageVersion="DEV" -DjavascriptVersion="LATEST_RELEASE" -DjavaVersion="LATEST_RELEASE" -Dsonar.runtimeVersion="LTS_OR_OLDEST_COMPATIBLE" -Dmaven.test.redirectTestOutputToFile=false install
   ;;
 
 esac
