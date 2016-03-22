5e67644c3c8ba466072edbeea32183d49aebb1b3
Tom <thomas.verin@sonarsource.com>
2016-03-22 16:39:31 +0100

    Setup QA

---
+++ cix.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+echo "Running $TEST with SQ=$SQ_VERSION"
+
+case "$TEST" in
+  plugin)  
+    
+  cd its/$TEST
+  mvn -DjavascriptVersion="LATEST_RELEASE" -DjavaVersion="LATEST_RELEASE" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false test
+  ;;
+
+  *)
+  echo "Unexpected TEST mode: $TEST"
+  exit 1
+  ;;
+esac
+
--- travis.sh
+++ travis.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 function installTravisTools {
   mkdir ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v21 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v27 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 
@@ -12,71 +12,25 @@ function strongEcho {
   echo ""
   echo "================ $1 ================="
 }
-
+installTravisTools
 case "$TARGET" in
 
 CI)
-  if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    strongEcho 'Build and analyze commit in master'
-    # this commit is master must be built and analyzed (with upload of report)
-    mvn verify -B -e -V
-
-    # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
-    source $HOME/.jdk_switcher_rc
-    jdk_switcher use oraclejdk8
-
-    # integration of jacoco report is quite memory-consuming
-    export MAVEN_OPTS="-Xmx1G -Xms128m"
-    mvn org.jacoco:jacoco-maven-plugin:prepare-agent verify -Pcoverage-per-test sonar:sonar -B -e -V \
-       -Dsonar.host.url=$SONAR_HOST_URL \
-       -Dsonar.login=$SONAR_TOKEN
-
-  elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
-    # For security reasons environment variables are not available on the pull requests
-    # coming from outside repositories
-    # http://docs.travis-ci.com/user/pull-requests/#Security-Restrictions-when-testing-Pull-Requests
-    # That's why the analysis does not need to be executed if the variable SONAR_GITHUB_OAUTH is not defined.
-    strongEcho 'Build and analyze pull request'
-    # this pull request must be built and analyzed (without upload of report)
-    mvn verify -B -e -V
-
-    # Switch to java 8 as the Dory HTTPS certificate is not supported by Java 7
-    source $HOME/.jdk_switcher_rc
-    jdk_switcher use oraclejdk8
-
-    export MAVEN_OPTS="-Xmx1G -Xms128m"
-    mvn verify sonar:sonar -B -e -V \
-        -Dsonar.analysis.mode=issues \
-        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.oauth=$GITHUB_TOKEN \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_TOKEN
-
-  else
-    strongEcho 'Build, no analysis'
-    # Build branch, without any analysis
-
-    # No need for Maven goal "install" as the generated JAR file does not need to be installed
-    # in Maven local repository
-    mvn verify -B -e -V
-  fi
+  SONAR_PROJECT_VERSION=`maven_expression "project.version"`
+ 
+  # Do not deploy a SNAPSHOT version but the release version related to this build
+  set_maven_build_version $TRAVIS_BUILD_NUMBER
+ 
+  # the profile "deploy-sonarsource" is defined in parent pom v28+
+  mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
+    -Pcoverage-per-test,deploy-sonarsource \
+    -Dmaven.test.redirectTestOutputToFile=false \
+    -Dsonar.host.url=$SONAR_HOST_URL \
+    -Dsonar.login=$SONAR_TOKEN \
+    -Dsonar.projectVersion=$SONAR_PROJECT_VERSION \
+    -B -e -V
   ;;
 
-IT)
-  installTravisTools
-  start_xvfb
-
-  mvn package -Dsource.skip=true -Denforcer.skip=true -Danimal.sniffer.skip=true -Dmaven.test.skip=true
-
-  if [ "${SQ_VERSION}" == "DEV" ];
-  then
-    build_snapshot "SonarSource/sonarqube"
-  fi
-
-  cd its/plugin
-  mvn -DjavascriptVersion="LATEST_RELEASE" -DjavaVersion="LATEST_RELEASE" -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false test
-  ;;
 
 *)
   echo "Unexpected TARGET value: $TARGET"
